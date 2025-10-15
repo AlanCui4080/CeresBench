9d91d3bd0a2a624c811d5db12880f19b007f18ff
AlanCui4080 <AlanCui4080@outlook.com>
2025-10-16 06:34:36 +0800

    init

---
+++ CeresBench/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace CeresBench.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ CeresBench/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace CeresBench.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
